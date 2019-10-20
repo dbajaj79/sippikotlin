f9888c63db8fc0d394c7beb3b7b2539e82f298b6
deepakbconsultpr <deepakb@consultpr.net>
2019-10-20 22:07:10 +0530

    Implement Palidrom Number

---
+++ src/PalidromNumber.kt
@@ -0,0 +1,21 @@
+fun main() {
+    println("Enter a Number")
+    var number = readLine()!!.toInt()
+    var reverse = 0
+    val tempNumber = number
+    while (number != 0) {
+        val digit = number % 10
+        reverse = reverse * 10 + digit
+        number = number / 10
+
+    }
+   if(tempNumber==reverse)
+   {
+       println("Enter Number $tempNumber is Palidrom Number")
+   }
+   else
+   {
+       println("Enter Number $tempNumber is not Palidrom Number")
+   }
+
+}
