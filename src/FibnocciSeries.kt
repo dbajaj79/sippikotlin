fcaf15ea81537c8453d643d96535e7e6d7901225
deepakbconsultpr <deepakb@consultpr.net>
2019-10-21 11:19:02 +0530

    Febnnocci Series Implementation Done

--- src/FibnocciSeries.kt
+++ src/FibnocciSeries.kt
@@ -1,4 +1,22 @@
 fun main() {
     println("Enter a number")
-    
+    var t2=1
+    var t1=0
+    var nextTerm=0
+    val number = readLine()!!.toInt()
+    if(number<=1)
+    {
+        println(number)
+    }
+    else
+    {
+
+        for(i in 1..number)
+        {
+          print("$t1,")
+          nextTerm = t1+t2
+            t1=t2
+            t2=nextTerm
+        }
+    }
 }
